45ae015aa07c3ee7e6a2e42d73227faaab578e44
Matheus Fernandes <matheusfernandesm2411@gmail.com>
2026-02-24 21:57:44 -0300

    FUNCOES NOVAS

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ function calcular(op){
 	if(op == '-') r = n1 - n2;
 	if(op == '*') r = n1 * n2;
 	if(op == '/') r = n1 / n2;
-	
+	if(op == 'MED') r = (n1 + n2) / 2
+	if(op == 'COM') r = (n1 * n2) / 100; //VALOR DA VENDA * PERCENTUAL
+	if(op == 'LC') r = n1 - n2; //PREÇO DE VENDA - PREÇO DE CUSTO
+
+	if(op == 'IMC'){
+		r = n1 / (n2 * n2);
+
+		if (r < 18.5)
+			resultado.innerHTML = "IMC: " + r.toFixed(2) + "<br>Abaixo do peso!";
+		else if (r < 25)
+			resultado.innerHTML = "IMC: " + r.toFixed(2) + "<br>Peso normal";
+		else
+			resultado.innerHTML = "IMC: " + r.toFixed(2) + "<br>Acima do peso!";
+		
+		return;
+	}
 	resultado.innerHTML= "Resultado: " + r;
 }
+
